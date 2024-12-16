cbb6c4f5838212739ab073f0a7636f3e23cb9b72
DoriaFabio <doriafabio2001@gmail.com>
2024-12-16 23:05:28 +0100

    modify css box

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,13 +5,13 @@ function Button() {
     const lang = [...languages];
     const [activelang, setActivelang] = useState(lang[0]);
     return (
-        <div className="m-4">
+        <div className="mx-4">
             <ul className="d-flex unstyle">
                 {lang.map((lang) => (
                     <li key={lang.id} onClick={() => setActivelang(lang)} className='btn btn-primary mx-3'>{lang.title}</li>
                 ))}
             </ul>
-            <div className="box rounded mt-5">
+            <div className="box rounded mt-4">
                 <h3>{activelang.title}</h3>
                 <p>{activelang.description}</p>
             </div>
